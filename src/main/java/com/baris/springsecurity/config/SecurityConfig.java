46b518251acdda45d8a3d7272a01918090b53e94
Barış Can Akdağ <bariscan.akdag155@gmail.com>
2019-04-17 13:45:09 +0300

    Bcrypt olarak şifreler tutuldu

--- src/main/java/com/baris/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/baris/springsecurity/config/SecurityConfig.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-       //use jdbc auth
+        //use jdbc auth
 
         auth.jdbcAuthentication().dataSource(securityDataSource);
     }
